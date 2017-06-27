4399fa0bbd4a59f434215ceb544d25026721b1c5
cheeseandham <softboxgames@gmail.com>
2017-06-27 23:23:38 +0100

    terrain setup, charater control setup

---
+++ Tree_House/Assets/C#/Charater_Control.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charater_Control : MonoBehaviour {
+
+    Rigidbody rb;
+
+    public float movementSpeed;
+    public float xSpeed;
+    public float ySpeed;
+
+    Camera mainCam;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        mainCam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 playerPos;
+
+        playerPos = transform.localPosition;
+
+        playerPos += transform.right * Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        playerPos += transform.forward * Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+
+        rb.MovePosition(playerPos);
+
+        float rotationX = Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
+        transform.Rotate(0.0f, rotationX, 0.0f);
+        
+        float rotationY = Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
+        mainCam.transform.Rotate(-rotationY, 0.0f, 0.0f);
+        
+    }
+}
